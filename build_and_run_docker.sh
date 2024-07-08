9dfed12805001324cb05a8f1c60947d361c3fd54
yucrazing <723284893@qq.com>
2024-07-08 18:36:52 +0800

    Update docker env

--- build_and_run_docker.sh
+++ build_and_run_docker.sh
@@ -2,7 +2,10 @@
 # docker build -t autodef-0 -f Dockerfile.0 .
 docker build -t autodef-1 -f Dockerfile.1 .
 
-# docker run -it autodef-1
+# docker run -it --rm autodef-1
 # docker run -it --rm -v "$(pwd)":/AutoDef my-custom-ubuntu
+
 # xhost +local:docker
-docker run --gpus all -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix autodef-1
+docker run -it --rm --name autodef-1 --gpus all -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix autodef-1
+# docker exec -it autodef-1 bash 
+# ./src/AutoDefRuntime/build/bin/AutoDefRuntime models/X_example
