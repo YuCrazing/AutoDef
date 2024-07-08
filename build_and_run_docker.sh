34e8a60d7066c544cb1555bf49139ce5742e2257
yucrazing <723284893@qq.com>
2024-07-08 13:49:45 +0800

    Add workable docker image

--- build_and_run_docker.sh
+++ build_and_run_docker.sh
@@ -1,11 +1,8 @@
 # docker build --no-cache -t my-custom-ubuntu .
-docker build -t autodef .
-# docker run -it autodef
-# docker run -it --rm -v "$(pwd)":/AutoDef my-custom-ubuntu
+# docker build -t autodef-0 -f Dockerfile.0 .
+docker build -t autodef-1 -f Dockerfile.1 .
 
-xhost +local:docker
-docker run -it \
-    --env="DISPLAY" \
-    --env="LIBGL_ALWAYS_INDIRECT=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    autodef
+# docker run -it autodef-1
+# docker run -it --rm -v "$(pwd)":/AutoDef my-custom-ubuntu
+# xhost +local:docker
+docker run --gpus all -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix autodef-1
